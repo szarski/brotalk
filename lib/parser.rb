986d192c28070d30587b630cc8978dd0f246fada
Jacek Szarski <jacek@applicake.com>
2012-05-15 10:35:42 +0200

    implemented basic Parser#react

--- lib/parser.rb
+++ lib/parser.rb
@@ -4,4 +4,13 @@ class Parser
   def initialize(parent)
     @parent = parent
   end
+
+  def react(sender, message)
+    if message == "ay bro"
+      parent.register_peer sender
+      return "ay"
+    else
+      return "u mad bro"
+    end
+  end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,9 +2,26 @@ require "spec_helper"
 
 describe Parser do
   let(:parent) {mock}
-  it "should initialize a new instance" do
-    Parser.new(parent).parent.should == parent
+    describe "#initialize" do
+    it "should initialize a new instance" do
+      Parser.new(parent).parent.should == parent
+    end
   end
 
   subject {Parser.new(parent)}
+  let(:sender) {mock}
+
+  describe "#react" do
+    it "should understand greetings" do
+      parent.should_receive(:register_peer).with(sender)
+      response = subject.react sender, "ay bro"
+      response.should == "ay"
+    end
+
+    it "should react to gibberish" do
+      parent.should_not_receive(:register_peer)
+      response = subject.react sender, "whay yuuuuu no tell me dat im preeetty"
+      response.should == "u mad bro"
+    end
+  end
 end

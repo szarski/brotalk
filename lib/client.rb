ceb5f88e1f84c693544bfb6ee1fd5d538ba6e0d8
Jacek Szarski <jacek@applicake.com>
2012-05-21 20:09:48 +0200

    implemented Client#initialize

--- lib/client.rb
+++ lib/client.rb
@@ -1,2 +1,10 @@
 class Client
+  attr_reader :communicator, :translator_receiver, :translator_sender
+
+  def initialize
+    @communicator = Communicator.new
+    @translator_receiver = Translator::Receiver.new(self)
+    @communicator.register_listener translator_receiver
+    @translator_sender = Translator::Sender.new(communicator)
+  end
 end
--- spec/lib/client_spec.rb
+++ spec/lib/client_spec.rb
@@ -4,11 +4,37 @@ require File.join(File.dirname(__FILE__), '../spec_helper')
 describe Client do
 
   describe "#initialize" do
-    it "initializes the communicator"
-    it "initializes the translator receiver with itself as a parent"
-    it "tells communicator to register translator receiver as a listener"
-    it "initializes the translator sender, gives it the communicator on initialization and stores a handler"
-    it "initializes the twitter_wrapper"
+    let(:communicator) {mock}
+    let(:translator_receiver) {mock}
+    let(:translator_sender) {mock}
+
+    it "initializes the communicator" do
+      Communicator.stub(:new).with.and_return communicator
+      communicator.stub(:register_listener)
+      described_class.new.communicator.should == communicator
+    end
+
+    it "initializes the translator receiver with itself as a parent" do
+      Translator::Receiver.stub(:new).with{|client| client.is_a?(Client)}.and_return translator_receiver
+      described_class.new.translator_receiver.should == translator_receiver
+    end
+
+    it "tells communicator to register translator receiver as a listener" do
+      Communicator.stub(:new).and_return communicator
+      Translator::Receiver.stub(:new).and_return translator_receiver
+      communicator.should_receive(:register_listener).with(translator_receiver)
+      described_class.new
+    end
+
+    it "initializes the translator sender, gives it the communicator on initialization and stores a handler" do
+      Translator::Sender.stub(:new).with(communicator).and_return translator_sender
+      Client.any_instance.stub(:communicator).and_return communicator
+      described_class.new.translator_sender.should == translator_sender
+    end
+
+    it "initializes the twitter_wrapper" do
+      pending "we don't need this yet"
+    end
   end
 
   describe "#start_with_twitter" do

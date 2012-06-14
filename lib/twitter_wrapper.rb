484d841e7370fb1d638501280bc0b620a1acd1e5
Mateusz Zawisza <mateusz@applicake.com>
2012-06-15 00:03:19 +0200

    implements TwitterWrapper with specs

--- lib/twitter_wrapper.rb
+++ lib/twitter_wrapper.rb
@@ -1,2 +1,41 @@
 class TwitterWrapper
+  def initialize
+    load_config
+    @twitter = Twitter::Client.new
+  end
+
+  def scan_addresses(hashtag)
+    twitter_messages = scan_twitter(hashtag)
+    fetch_addresses(twitter_messages)
+  end
+
+  def publish_address(ip_address, hashtag)
+    @twitter.update("And now something completly different "\
+      "#{ip_address} ##{hashtag}")
+  end
+
+  private
+    def scan_twitter(hashtag)
+      @twitter.search("##{hashtag}").map(&:full_text)
+    end
+
+    def fetch_addresses(twitter_messages)
+      twitter_messages.map{|mesg| mesg.scan(regexp)}.flatten.uniq
+    end
+
+    def regexp
+      /(?:\d{1,3}\.){3}\d{1,3}/
+    end
+
+    def load_config
+      twitter_config = YAML.load_file(File.join(File.dirname(__FILE__),
+        "../config/twitter.yml"))
+      Twitter.configure do |config|
+        config.consumer_key = twitter_config["consumer_key"]
+        config.consumer_secret = twitter_config["consumer_secret"]
+        config.oauth_token = twitter_config["oauth_token"]
+        config.oauth_token_secret = twitter_config["oauth_token_secret"]
+      end
+    end
+  #EOF private
 end
--- spec/lib/twitter_wrapper_spec.rb
+++ spec/lib/twitter_wrapper_spec.rb
@@ -1,11 +1,45 @@
 require File.join(File.dirname(__FILE__), '../spec_helper')
 
 describe TwitterWrapper do
+  let(:twitter_mock) {mock("TwitterMock")}
+  let(:yaml_mock) {mock("Yaml#load_file").as_null_object}
+
+  before(:each) do
+    YAML.stub(:load_file).and_return(yaml_mock)
+  end
+
   describe "#publish_address" do
-    it "should post gived address to twitter with the proper hashtag"
+    let(:hashtag) {"brotalk"}
+    let(:ip_address) {"1.2.3.4"}
+    let(:message) {"And now something completly different #{ip_address} ##{hashtag}"}
+    let(:twitter_update_mock) {mock("TwitterMock#update")}
+
+    before(:each) do
+      twitter_mock.stub(:update).with(message).and_return(twitter_update_mock)
+      Twitter::Client.stub(:new).and_return(twitter_mock)
+    end
+
+    it "invokes Twitter::Client.update with message" do
+      twitter_mock.should_receive(:update).with(message)
+      subject.publish_address(ip_address, hashtag)
+    end
   end
 
   describe "#scan_addresses" do
-    it "should scan given hashtag and retrieve an array of peer addresses"
+    let(:twitter_search_mock) {mock("Twitter#search", :map=>["adssda1.2.3.4asdsda"])}
+
+    before(:each) do
+      twitter_mock.stub(:search).with("#brotalk").and_return(twitter_search_mock)
+      Twitter::Client.stub(:new).and_return(twitter_mock)
+    end
+
+    it "invokes Twitter::Client#search" do
+      twitter_mock.should_receive(:search).with("#brotalk")
+      subject.scan_addresses("brotalk")
+    end
+
+    it "retrieves an array of peer addresses when given a hashtag" do
+      subject.scan_addresses("brotalk").should eq(["1.2.3.4"])
+    end
   end
 end

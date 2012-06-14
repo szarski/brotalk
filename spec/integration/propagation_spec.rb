35e086ce028c8244396ff9741ab77ef5986c296c
Jacek Szarski <jacek@applicake.com>
2012-06-14 18:20:11 +0200

    simplified the propagation spec

--- spec/integration/propagation_spec.rb
+++ spec/integration/propagation_spec.rb
@@ -24,19 +24,19 @@ describe "propagation" do
 
   scenario "some of the bros are given the same bros address, only they know only each other" do
     clients.map &:start_listening
-    clients[-2].greet Communicator.listeners.keys.last
-    clients[-1].greet Communicator.listeners.keys.last
-    clients[-5].greet Communicator.listeners.keys.last
+    clients[2].greet Communicator.listeners.keys.last
+    clients[1].greet Communicator.listeners.keys.last
+    clients[5].greet Communicator.listeners.keys.last
 
-    bros_indexes = [-1,-2,-5]
-    other_bros_indexes = ((-10..-1).to_a - [-1,-2,-5])
-    clients.values_at(*bros_indexes).select {|c| c.bros_table.sort == Communicator.listeners.keys.values_at(*bros_indexes).sort}.count.should == 3
-    clients.values_at(*other_bros_indexes).select {|c| c.bros_table.empty?}.count.should == 7
+    bros_indexes = [1,2,5,9]
+    other_bros_indexes = ((0..9).to_a - [1,2,5])
+    clients.values_at(*bros_indexes).select {|c| c.bros_table.sort == Communicator.listeners.keys.values_at(*bros_indexes).sort}.count.should == 4
+    clients.values_at(*other_bros_indexes).select {|c| c.bros_table.empty?}.count.should == 6
   end
 
   scenario "each bro is given the last bro's address, everyone knows everybody" do
     clients.map &:start_listening
     clients.map {|c| c.greet Communicator.listeners.keys.last}
-    clients.select {|c| c.bros_table.sort == Communicator.listeners.keys.values_at(*(-10..-1).to_a).sort}.count.should == 10
+    clients.select {|c| c.bros_table.sort == Communicator.listeners.keys.sort}.count.should == 10
   end
 end

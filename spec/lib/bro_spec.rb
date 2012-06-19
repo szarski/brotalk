15f840061e3a649f153253bafcf7b10b891f568c
Jacek Szarski <jacek@applicake.com>
2012-06-19 23:05:43 +0200

    implemented Bro#to_json #<=> and made .from_json raise on invalid json

--- spec/lib/bro_spec.rb
+++ spec/lib/bro_spec.rb
@@ -22,14 +22,31 @@ describe Bro do
       bro_a1.should_not == bro_b
 
       [bro_a1, bro_a2, bro_b].uniq.should == [bro_a1, bro_b]
+
+      [bro_a1, bro_b].should == [bro_a1, bro_b]
+      [bro_a1, bro_b].should_not == [bro_a1, bro_a1]
+    end
+  end
+
+  describe "<=>" do
+    it "should allow sorting" do
+      lambda {[Bro.new('aaa',false), Bro.new('bbb',true)].sort}.should_not raise_error
+    end
+  end
+
+  describe "#to_json" do
+    it "transforms properly" do
+      Bro.new('abc', false).to_json.should == "abc%0"
+      Bro.new('abc', true).to_json.should == "abc%1"
     end
   end
 
   describe ".from_json" do
-    let(:bros_json) {[mock, mock, mock]}
-    let(:bros) {[mock, mock, mock]}
+    let(:bros_json) {%w{aaa%0 bbb%1 ccc%0}}
+    let(:bros) {[Bro.new('aaa',false),Bro.new('bbb',true),Bro.new('ccc',false)]}
+
     it "should load from an array" do
-      bros_json.each_with_index {|bro, index| Bro.stub(:new).with(bro).and_return(bros[index])}
+      #bros_json.each_with_index {|bro, index| Bro.stub(:new).with(bro).and_return(bros[index])}
       described_class.from_json(bros_json).should == bros
     end
   end

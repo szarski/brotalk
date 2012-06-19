ecebf33bfed59a60c6d319c57bbb989c4b69562e
Jacek Szarski <jacek@applicake.com>
2012-06-19 22:27:31 +0200

    implemented Bro equality operators

--- spec/lib/bro_spec.rb
+++ spec/lib/bro_spec.rb
@@ -10,4 +10,17 @@ describe Bro do
       bro.supernode?.should == supernode
     end
   end
+
+  describe "#eql? and related" do
+    it "should compare the address only" do
+      bro_a1 = Bro.new('a', false)
+      bro_a2 = Bro.new('a', true)
+      bro_b = Bro.new('b', false)
+
+      bro_a1.should == bro_a2
+      bro_a1.should_not == bro_b
+
+      [bro_a1, bro_a2, bro_b].uniq.should == [bro_a1, bro_b]
+    end
+  end
 end

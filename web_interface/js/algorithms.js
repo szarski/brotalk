9e11029002bf9d254a27595a3053d40e0b866c92
Jacek Szarski <jacek@applicake.com>
2012-06-20 21:04:51 +0200

    implemented a simple html interface

---
+++ web_interface/js/algorithms.js
@@ -0,0 +1,38 @@
+var redraw;
+
+window.onload = function() {
+
+$.get('/clients.json', draw);
+
+
+}
+
+function draw(data) {
+  alert(data);
+    var width = $(document).width();
+    var height = $(document).height() - 100;
+
+    var g = new Graph();
+    
+
+
+
+    /* creating nodes and passing the new renderer function to overwrite the default one */
+
+
+    _.each(JSON.parse(data), function(client){
+    g.addNode(client['address']);
+    });
+
+    /* layout the graph using the Spring layout implementation */
+    var layouter = new Graph.Layout.Spring(g);
+    
+    /* draw the graph using the RaphaelJS draw implementation */
+    var renderer = new Graph.Renderer.Raphael('canvas', g, width, height);
+
+    redraw = function() {
+        layouter.layout();
+        renderer.draw();
+    };
+    
+};

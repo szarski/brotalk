1a6588ab51e599d0b18f4a99b68fbd6896c1d49b
Jacek Szarski <jacek@applicake.com>
2012-06-20 20:35:13 +0200

    implemented a webtest simulator

---
+++ webtest.rb
@@ -0,0 +1,24 @@
+require File.join(File.dirname(__FILE__), 'lib', 'brotalk')
+require 'drb'
+
+class Simulator
+  def initialize
+    c1 = Client.new
+    c1.start_listening
+    c2 = Client.new
+    c2.start_listening
+  end
+
+  def listeners
+    Communicator::DEFAULT_CLASS.listeners
+  end
+
+  def greet(sender, recipient)
+    listeners[sender].listeners.first.client.greet recipient
+  end
+end
+
+simulator = Simulator.new
+
+DRb.start_service('druby://localhost:9000', simulator)
+DRb.thread.join

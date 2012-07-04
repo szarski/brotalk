a2bab8e77b9430b57d06255184ab00b6d539f830
Mateusz Zawisza <mateusz@applicake.com>
2012-07-04 20:42:06 +0200

    use UDP communicator in brotalk bin

--- bin/brotalk.rb
+++ bin/brotalk.rb
@@ -1,7 +1,7 @@
 ##! /bin/ruby
 require File.join(File.dirname(__FILE__), "../lib/brotalk.rb")
 
-client = Client.new
+client = Client.new(true)
 client.start_listening
 if address = ARGV[0]
   client.greet(address)

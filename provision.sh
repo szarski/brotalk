85434cd39b7ebffe8978c6694be5426c7ec6d7c4
Mateusz Zawisza <mateusz@applicake.com>
2012-07-04 17:55:45 +0200

    uncomment provisioning script commands

--- provision.sh
+++ provision.sh
@@ -1,8 +1,8 @@
-#apt-get install -y git-core build-essential curl vim screen
-#gem install bundler
-#
-#su vagrant -lc "curl -L https://get.rvm.io | bash -s stable --ruby"
-#su vagrant -lc "rvm install ruby 1.9.2"
-## do not clone. we can use shared dir
-##su vagrant -lc "[[ ! -d brotalk ]] && git clone git://github.com/szarski/brotalk.git"
+apt-get install -y git-core build-essential curl vim screen
+gem install bundler
+
+su vagrant -lc "curl -L https://get.rvm.io | bash -s stable --ruby"
+su vagrant -lc "rvm install ruby 1.9.2"
+# do not clone. we can use shared dir
+#su vagrant -lc "[[ ! -d brotalk ]] && git clone git://github.com/szarski/brotalk.git"
 su vagrant -lc "cd /vagrant && rvm 1.9.2@brotalk --create && bundle install"
